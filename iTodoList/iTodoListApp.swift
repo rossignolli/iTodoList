2010c39a46fa70c0571aebc7bb4d9602887271e4
Vitor Rossignolli <vitorrossignolli@gmail.com>
2022-03-31 21:25:03 -0300

    Initial Commit

---
+++ iTodoList/iTodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTodoListApp.swift
+//  iTodoList
+//
+//  Created by Vitor Vigarani Rossignolli on 31/03/22.
+//
+
+import SwiftUI
+
+@main
+struct iTodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
